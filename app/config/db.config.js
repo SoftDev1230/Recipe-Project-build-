9016e7f19ce17614f126f5986d2d0ca80075bce7
Rodrigo Hernandez <146174609+SoftDev1230@users.noreply.github.com>
2024-01-20 01:02:10 -0800

    Update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  HOST: "95.217.33.57:7777",
+  HOST: "95.217.33.57",
+  PORT: '7777',
   USER: "postgres",
   PASSWORD: "123456789",
   DB: "testdb",
   dialect: "postgres",
-  PORT: 5432,
   pool: {
     max: 5,
     min: 0,
